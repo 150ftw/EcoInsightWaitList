295b596a36e0bc093034c4742af38eacb3e025f5
Shivam Sharma <shivamsharma@Shivams-MacBook-Pro.local>
2026-03-16 18:13:24 +0530

    Fix: Enhance z-index for sub-page navigation links to prevent click interception

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Benefits from './components/Benefits'
@@ -23,40 +23,53 @@ const LandingPage = () => (
 function App() {
   return (
     <Router>
-      <div className="app-landing" style={{ position: 'relative' }}>
-        {/* Global Background Element */}
-        <div style={{
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            width: '100vw',
-            height: '100vh',
-            zIndex: -1,
-            pointerEvents: 'none',
-            overflow: 'hidden'
-        }}>
-          <DarkVeil
-            hueShift={0}
-            noiseIntensity={0.03}
-            scanlineIntensity={0.1}
-            speed={0.3}
-            scanlineFrequency={2}
-            warpAmount={0.05}
-            resolutionScale={1}
-          />
-        </div>
-
-        <Navbar />
-        
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/info/:slug" element={<InfoPage />} />
-        </Routes>
-
-        <Footer />
-      </div>
+      <AppContent />
     </Router>
   )
+}
+
+function AppContent() {
+  const location = useLocation();
+  
+  useEffect(() => {
+    console.log('🛣️ ROUTE CHANGED:', location.pathname);
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return (
+    <div className="app-landing" style={{ position: 'relative' }}>
+      {/* Global Background Element */}
+      <div style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100vw',
+          height: '100vh',
+          zIndex: -1,
+          pointerEvents: 'none',
+          overflow: 'hidden'
+      }}>
+        <DarkVeil
+          hueShift={0}
+          noiseIntensity={0.03}
+          scanlineIntensity={0.1}
+          speed={0.3}
+          scanlineFrequency={2}
+          warpAmount={0.05}
+          resolutionScale={1}
+        />
+      </div>
+
+      <Navbar />
+      
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/info/:slug" element={<InfoPage />} />
+      </Routes>
+
+      <Footer />
+    </div>
+  );
 }
 
 export default App
